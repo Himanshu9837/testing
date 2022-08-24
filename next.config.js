be60ee1ea7e87be92e217d60f60c061bb0cfcb39
Himanshu Sharma <himanshu.sharma@madstech.com>
2022-08-24 10:48:04 +0530

    Commit message 52

--- next.config.js
+++ next.config.js
@@ -12,51 +12,18 @@
 
 
 // }
-const path = require("path");
-const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"],  plugins: ["transform-class-properties"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    historyApiFallback: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
-};
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
-}
+// module.exports = {
+//   plugins: {
+//     autoprefixer: {},
+//   },
+// }
 
-module.exports = { trailingSlash: true, }
+// module.exports = { trailingSlash: true, }
 
-module.exports = {
-  basePath: '/public/static/images',
-}
+// module.exports = {
+//   basePath: '/public/static/images',
+// }
 
 // module.exports = {
 //   webpack: (config, { isServer }) => {
@@ -105,12 +72,12 @@ module.exports = {
 //   }
 // };
 
-module.exports = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-}
+// module.exports = {
+//   images: {
+//     loader: 'akamai',
+//     path: '',
+//   },
+// }
 
 
 // module.exports = {
